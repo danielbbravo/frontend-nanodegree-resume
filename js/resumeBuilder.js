030cce74c5fe0d6cc4c3dd9bed27d9e33881c7db
Daniel Bravo <danielbravo1@Daniels-MBP.lan>
2016-01-30 18:56:49 +0000

    Add.
    Add.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,14 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- var name = "Daniel Bravo";
+var name = "Daniel Bravo";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+$("#header").append(formattedName);
+
+var role = "Front End Developer";
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedRole);
